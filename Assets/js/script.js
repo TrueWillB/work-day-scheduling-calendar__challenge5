a59e5e8980c0387cfa8d488f385bc4615ecd9911
William Brown <willis291@gmail.com>
2023-05-29 21:14:09 -0600

    Added comments to code to clarify meaning of some sections

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -35,33 +35,14 @@ $(function () {
   setUpCurrentTimeDisplay();
   populateCalender();
 
-  // console.log(timeArray);
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
 
+
+// This is the click listener for the save buttons on the calendar
 allEvents.on("click", ".saveBtn", function (event) {
-  event.preventDefault();
+  event.preventDefault();Your location
   var thisTimeElement = $(this).parent(); //convenience variable
   var thisTimeNum = $(thisTimeElement).attr("data-hourNum");
-  // console.log(thisTimeNum);
   var eventTextToSave = thisTimeElement.find(".description").val(); //This saves the value typed within the calendar event into the variable calendarEvent. this.parent goes up from the clicked button to the parent div element, then .find(".description")
 
   //okay, this variable was way overkill. I could have just assumed the size of the calendar would remain the same forever, and that I could always point to an index and always point to the same spot, but I kind of wanted this to be dynamic so that a future programmer could just add more hours to the calendar in the future and not really have to change the js code at all. As long as the coder adds the element with the data attribute of its hour number, this js should never have to be touched. I also can't help but think that there was probably a more direct way of doing this, and that I'll facepalm when I realize it. Hey, I learned about maps and got practice, so isn't that the most important thing?
@@ -71,8 +52,6 @@ allEvents.on("click", ".saveBtn", function (event) {
       return eventObj.hourNum;
     })
     .indexOf(thisTimeNum);
-
-  console.log("Will save " + saveIndex + " and text: " + eventTextToSave);
 
   saveEventToLocal(saveIndex, eventTextToSave);
 });
@@ -92,12 +71,14 @@ function setUpCurrentTimeDisplay() {
   }
 }
 
+//This function actually takes the event and saves it into local memory so that it can be loaded again upon opening the page
 function saveEventToLocal(saveIndex, eventTextToSave) {
   timeArray[saveIndex].eventText = eventTextToSave;
   console.log(JSON.stringify(timeArray));
   localStorage.setItem("savedEvents", JSON.stringify(timeArray));
 }
 
+//This function takes the events saved into timeArray and actually plaves them into the calendar on display on the website
 function populateCalender() {
   var eventToFill;
   for (i = 0; i < timeArray.length; i++) {
